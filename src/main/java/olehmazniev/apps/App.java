ecde40c65a9bf609ac18358bc95e4772f200f742
Oleh <zaaqq135@gmail.com>
2024-04-26 00:21:18 +0300

    started app

--- src/main/java/olehmazniev/apps/App.java
+++ src/main/java/olehmazniev/apps/App.java
@@ -1,13 +1,21 @@
 package olehmazniev.apps;
 
+import javax.swing.SwingUtilities;
+
 /**
- * Hello world!
+ * @author Oleh Mazniev
+ * @version 1.0
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new WeatherAppGui().setVisible(true);
+            }
+
+        });
     }
 }
---
+++ src/main/java/olehmazniev/apps/WeatherAppGui.java
@@ -0,0 +1,16 @@
+package olehmazniev.apps;
+
+import javax.swing.*;
+
+public class WeatherAppGui extends JFrame {
+
+    public WeatherAppGui() {
+        super("Weather App");
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(450, 650);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+    }
+}
