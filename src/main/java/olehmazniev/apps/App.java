a8c23c2a5770729254e5a254e7539501e529a2f5
Oleh <zaaqq135@gmail.com>
2024-04-27 11:50:48 +0300

    added gui components

--- src/main/java/olehmazniev/apps/App.java
+++ src/main/java/olehmazniev/apps/App.java
@@ -3,12 +3,24 @@ package olehmazniev.apps;
 import javax.swing.SwingUtilities;
 
 /**
+ * The {@code App} class serves as the main entry point for the WeatherApp. This
+ * class contains the main method which initializes and displays the WeatherApp
+ * GUI by invoking {@link WeatherAppGui} on the Event Dispatch Thread (EDT) to
+ * ensure thread safety in Swing components.
+ *
  * @author Oleh Mazniev
  * @version 1.0
- *
  */
 public class App {
 
+    /**
+     * Launches the WeatherApp GUI. This is the entry point of the application.
+     * The {@code main} method schedules the application's GUI for creation and
+     * visibility on the Swing Event Dispatch Thread, ensuring that all UI
+     * updates are safely executed on the EDT.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new WeatherAppGui().setVisible(true);
--- src/main/java/olehmazniev/apps/WeatherAppGui.java
+++ src/main/java/olehmazniev/apps/WeatherAppGui.java
@@ -1,9 +1,18 @@
 package olehmazniev.apps;
 
 import java.awt.Font;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 public class WeatherAppGui extends JFrame {
 
@@ -20,11 +29,51 @@ public class WeatherAppGui extends JFrame {
     }
 
     private void addGuiComponents() {
+        // search field
         JTextField searchTextField = new JTextField();
-
         searchTextField.setBounds(15, 15, 351, 45);
         searchTextField.setFont(new Font("Dialog", Font.PLAIN, 24));
         add(searchTextField);
-    
+
+        // search button
+        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+        searchButton.setBounds(375, 13, 47, 45);
+        add(searchButton);
+
+        // weather image
+        JLabel weatherConditionImage = new JLabel(loadImage("src/assets/rain.png"));
+        weatherConditionImage.setBounds(0, 125, 450, 217);
+        add(weatherConditionImage);
+
+        // text temperature
+        JLabel temperatureText = new JLabel("10 C");
+        temperatureText.setBounds(0, 350, 450, 54);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD, 48));
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        JLabel weatherConditionDesc = new JLabel("Cloudy");
+        weatherConditionDesc.setBounds(0, 405, 450, 36);
+        weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN, 32));
+        weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDesc);
+
     }
+    
+
+    
+    private Icon loadImage(String resourcePath) {
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+
+            return new ImageIcon(image);
+
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+
+        System.out.println("Could not find resource!");
+        return null;
+    }
+
 }
